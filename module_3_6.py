c29b657b18cacc796a98fe9404cc5ee4c6684175
Ruslan <daminov.ruslan@gmail.com>
2024-12-09 21:21:25 +0500

    Update module_3_6.py

--- module_3_6.py
+++ module_3_6.py
@@ -1,16 +1,21 @@
 def calculate_structure_sum(data_structure):
     summa = 0
+
     if isinstance(data_structure, dict):
         for key, value in data_structure.items():
             summa += calculate_structure_sum(key)
             summa += calculate_structure_sum(value)
+
     elif isinstance(data_structure, (list, tuple, set)):
         for item in data_structure:
             summa += calculate_structure_sum(item)
+
     elif isinstance(data_structure, (int, float)):
         summa += data_structure
+
     elif isinstance(data_structure, str):
         summa += len(data_structure)
+
     return summa
 
 data_structure = [
